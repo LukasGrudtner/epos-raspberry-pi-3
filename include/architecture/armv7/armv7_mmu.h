0cda9757a585f657747729d30e4c95c40d188be7
Lukas Derner Grüdtner <lukasgrudtner@hotmail.com>
2021-08-08 21:33:11 -0300

    Fix MMU with ARMv7 flags

--- include/architecture/armv7/armv7_mmu.h
+++ include/architecture/armv7/armv7_mmu.h
@@ -30,23 +30,38 @@ public:
     class Page_Flags {
     public:
         enum {
-            V = 1 << 0, // Valid
-            R = 1 << 1, // Readable
-            W = 1 << 2, // Writable
-            X = 1 << 3, // Executable
-            U = 1 << 4, // User accessible
-            G = 1 << 5, // Global (mapped in multiple PTs)
-            A = 1 << 6, // Accessed
-            D = 1 << 7, // Dirty
-            CT = 1 << 8, // Contiguous (reserved for use by supervisor RSW)
+            XN = 1 << 0,
+            P = 1 << 1,
+            B = 1 << 2,
+            C = 1 << 3,
+            APX = 1 << 9,
+            UMPREV_ACCESS = 1 << 5,
+            RW_UMPREV = 1 << 4,
+            S = 1 << 10,
+            NG = 1 << 11,
+            PRIVILED_ACCESS_ONLY = 1 << 4,
+            NO_USER_MODE_WRITE = 1 << 5,
+            FULL_ACCESS = (1 << 4) | (1 << 5),
+            PRIVILEGED_READ_ONLY = (1 << 9) | (1 << 4),
+            READ_ONLY = (1 << 9) | (1 << 5),
+            STRONGLY_ORDERED = 0,
+            SHAREABLE_DEVICE = B | XN,
+            NORMAL_1 = C,                         // Outer and Inner write-through, no allocate on write
+            NORMAL_2 = B | C,                     // Outer and Inner write-back, no allocate on write
+            NORMAL_3 = 1 << 6,                    // Outer and Inner non-cacheable
+            NON_SHAREABLE_DEVICE = (1 << 7) | XN, // Outer and Inner non-cacheable
+            V = 1 << 0,   // Valid
+            R = 1 << 1,   // Readable
+            W = 1 << 2,   // Writable
+            X = 1 << 3,   // Executable
+            U = 1 << 4,   // User accessible
             MIO = 1 << 9, // I/O (reserved for use by supervisor RSW)
-            APP = (V | R | W | X | U),
-            APPC = (V | R | X | U),
-            APPD = (V | R | W | U),
             SYS = (V | R | W | X),
+            APP = (V | R | W | X | U),
             IO = (SYS | MIO),
-            DMA = (SYS | CT),
-            MASK = (1 << 10) - 1
+            MASK_PDE = (1 << 10) - 1,
+            MASK_PTE = (1 << 12) - 1,
+            DMA = (SYS | Flags::CT),
         };
 
     public:
@@ -54,15 +69,16 @@ public:
 
         Page_Flags(unsigned int f) : _flags(f) {}
 
-        Page_Flags(Flags f) : _flags(V |
-                                     ((f & Flags::RD) ? R : 0) |
-                                     ((f & Flags::RW) ? W : 0) |
-                                     ((f & Flags::EX) ? X : 0) |
-                                     ((f & Flags::USR) ? U : 0) |
-                                     ((f & Flags::CWT) ? 0 : 0) |
-                                     ((f & Flags::CD) ? 0 : 0) |
-                                     ((f & Flags::CT) ? CT : 0) |
-                                     ((f & Flags::IO) ? IO : 0)) {}
+        Page_Flags(Flags f) : _flags(P |
+                                     ((f & Flags::RD) ? 0 : 0) |
+                                     ((f & Flags::RW) ? RW_UMPREV : 0) |
+                                     ((f & Flags::EX) ? 0 : XN) |
+                                     ((f & Flags::USR) ? UMPREV_ACCESS : 0) |
+                                     ((f & Flags::CWT) ? NORMAL_1 : NORMAL_2) |
+                                     ((f & Flags::CD) ? NORMAL_3 : 0) |
+                                     ((f & Flags::CT) ? 0 : 0) |
+                                     ((f & Flags::IO) ? 0 : 0) |
+                                     ((f & Flags::SYS) ? APX : 0)) {}
 
         operator unsigned int() const { return _flags; }
 
@@ -134,7 +150,62 @@ public:
     };
 
     // Page Directory
-    typedef Page_Table Page_Directory;
+    class Page_Directory {
+    public:
+        Page_Directory() {}
+
+        PT_Entry &operator[](unsigned int i) { return _entry[i]; }
+
+        Page_Directory &log() { return *static_cast<Page_Directory *>(phy2log(this)); }
+
+        void map(int from, int to, Page_Flags flags, Color color) {
+            Phy_Addr *addr = alloc(to - from, color);
+            if (addr)
+                remap(addr, from, to, flags);
+            else
+                for (; from < to; from++) {
+                    Log_Addr *pte = phy2log(&_entry[from]);
+                    *pte = phy2pte(alloc(1, color), flags);
+                }
+        }
+
+        void map_contiguous(int from, int to, Page_Flags flags, Color color) {
+            remap(alloc(to - from, color), from, to, flags);
+        }
+
+        void remap(Phy_Addr addr, int from, int to, Page_Flags flags) {
+            addr = align_page(addr);
+            for (; from < to; from++) {
+                Log_Addr *pte = phy2log(&_entry[from]);
+                *pte = phy2pte(addr, flags);
+                addr += sizeof(Page);
+            }
+        }
+
+        void unmap(int from, int to) {
+            for (; from < to; from++) {
+                free(_entry[from]);
+                Log_Addr *pte = phy2log(&_entry[from]);
+                *pte = 0;
+            }
+        }
+
+        friend OStream &operator<<(OStream &os, Page_Directory &pt) {
+            os << "{\n";
+            int brk = 0;
+            for (unsigned int i = 0; i < PD_ENTRIES; i++)
+                if (pt[i]) {
+                    os << "[" << i << "]=" << pte2phy(pt[i]) << "  ";
+                    if (!(++brk % 4))
+                        os << "\n";
+                }
+            os << "\n}";
+            return os;
+        }
+
+    private:
+        PD_Entry _entry[PD_ENTRIES];
+    };
 
     // Chunk (for Segment)
     class Chunk {
@@ -144,10 +215,7 @@ public:
         Chunk(unsigned int bytes, Flags flags, Color color = WHITE)
                 : _from(0), _to(pages(bytes)), _pts(page_tables(_to - _from)), _flags(Page_Flags(flags)),
                   _pt(calloc(_pts, WHITE)) {
-            if (_flags & Page_Flags::CT)
-                _pt->map_contiguous(_from, _to, _flags, color);
-            else
-                _pt->map(_from, _to, _flags, color);
+            _pt->map(_from, _to, _flags, color);
         }
 
         Chunk(Phy_Addr phy_addr, unsigned int bytes, Flags flags)
@@ -160,13 +228,8 @@ public:
                 : _from(from), _to(to), _pts(page_tables(_to - _from)), _flags(flags), _pt(pt) {}
 
         ~Chunk() {
-            if (!(_flags & Page_Flags::IO)) {
-                if (_flags & Page_Flags::CT)
-                    free((*_pt)[_from], _to - _from);
-                else
-                    for (; _from < _to; _from++)
-                        free((*_pt)[_from]);
-            }
+            for (; _from < _to; _from++)
+                free((*_pt)[_from]);
             free(_pt, _pts);
         }
 
@@ -179,13 +242,10 @@ public:
         unsigned int size() const { return (_to - _from) * sizeof(Page); }
 
         Phy_Addr phy_address() const {
-            return (_flags & Page_Flags::CT) ? Phy_Addr(indexes((*_pt)[_from])) : Phy_Addr(false);
+            return (_flags & Flags::CT) ? Phy_Addr(indexes((*_pt)[_from])) : Phy_Addr(false);
         }
 
         int resize(unsigned int amount) {
-            if (_flags & Page_Flags::CT)
-                return 0;
-
             unsigned int pgs = pages(amount);
 
             Color color = colorful ? phy2color(_pt) : WHITE;
@@ -408,9 +468,9 @@ public:
     }
 
     static PT_Entry phy2pte(Phy_Addr frame, Page_Flags flags) { return (frame >> 2) | flags; }
-    static Phy_Addr pte2phy(PT_Entry entry) { return (entry & ~Page_Flags::MASK) << 2; }
+    static Phy_Addr pte2phy(PT_Entry entry) { return (entry & ~Page_Flags::MASK_PTE) << 2; }
     static PD_Entry phy2pde(Phy_Addr frame) { return (frame >> 2) | Page_Flags::V; }
-    static Phy_Addr pde2phy(PD_Entry entry) { return (entry & ~Page_Flags::MASK) << 2; }
+    static Phy_Addr pde2phy(PD_Entry entry) { return (entry & ~Page_Flags::MASK_PDE) << 2; }
 
     static void flush_tlb() {
         ASM("mov r0, #0x0");
