dd7a60f8605e79e5414e576b136a10008b279709
Filipe Borba <filipe.borba@grad.ufsc.br>
2021-08-31 10:12:30 -0300

    Add system_binding

---
+++ src/system/system_binding.cc
@@ -0,0 +1,18 @@
+#include <machine.h>
+#include <process.h>
+#include <system.h>
+
+// Bindings
+extern "C" {
+    __USING_SYS;
+
+    // Libc legacy
+    void _panic() { Machine::panic(); }
+    void _exit(int s) { Thread::exit(s); for(;;); }
+    void __exit() { Thread::exit(CPU::fr()); }  // must be handled by the Page Fault handler for user-level tasks
+    void __cxa_pure_virtual() { db<void>(ERR) << "Pure Virtual method called!" << endl; }
+
+    // Utility-related methods that differ from kernel and user space.
+    // OStream
+    void _print(const char * s) { Display::puts(s); }
+}
